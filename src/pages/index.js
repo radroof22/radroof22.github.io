7d0afc72416e895499d5f5ce4f272a8e70d67b4f
Rohan <mehta.rohan@outlook.com>
2021-05-23 12:00:43 -0400

    hello header on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <h1>Inde</h1>
+      <div className="container p-7 bg-night text-white">
+        <h1 className="text-4xl text-slime my-5">Hi, I am <span className="font-bold text-white">Rohan</span></h1>
+        <h2 className="text-3xl text-slime italic">Student, Developer, Innovator</h2>
+      </div>
     </Layout>
   )
 }
